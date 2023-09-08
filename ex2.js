c6825c5244c1d678b99665f43466e8a1f68f1a27
Yerbol Kuldeyev <yerbol.kuldeyev@gmail.com>
2023-09-07 22:57:43 -0700

    first com

--- ex2.js
+++ ex2.js
@@ -1,1 +1,23 @@
-lets sum = a+blur;
+function maxProfit(prices) {
+  let profit = 0;
+
+  for (let i = 1; i < prices.length; i++) {
+    const potentialProfit = prices[i] - prices[i - 1];
+
+    if (potentialProfit > 0) {
+      profit += potentialProfit;
+    }
+  }
+
+  return profit;
+}
+
+// Example usage:
+const prices1 = [7, 1, 5, 3, 6, 4];
+console.log(maxProfit(prices1)); // Output: 7
+
+const prices2 = [1, 2, 3, 4, 5];
+console.log(maxProfit(prices2)); // Output: 4
+
+const prices3 = [7, 6, 4, 3, 1];
+console.log(maxProfit(prices3)); // Output: 0
